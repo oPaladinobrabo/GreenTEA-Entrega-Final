02a302401cbf4dbacae66776474f19f3fd68cca6
zailua <84216764+zailua@users.noreply.github.com>
2022-03-23 14:56:07 -0300

    up

---
+++ Api/Api/testeApi001/Controllers/ReceitasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeApi001.Data;
+using testeApi001.Models;
+
+namespace testeApi001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceitasController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public ReceitasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Receitas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Receita>>> GetReceitas()
+        {
+            return await _context.Receitas.ToListAsync();
+        }
+
+        // GET: api/Receitas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Receita>> GetReceita(int id)
+        {
+            var receita = await _context.Receitas.FindAsync(id);
+
+            if (receita == null)
+            {
+                return NotFound();
+            }
+
+            return receita;
+        }
+
+        // PUT: api/Receitas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReceita(int id, Receita receita)
+        {
+            if (id != receita.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(receita).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReceitaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Receitas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Receita>> PostReceita(Receita receita)
+        {
+            _context.Receitas.Add(receita);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReceita", new { id = receita.Id }, receita);
+        }
+
+        // DELETE: api/Receitas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReceita(int id)
+        {
+            var receita = await _context.Receitas.FindAsync(id);
+            if (receita == null)
+            {
+                return NotFound();
+            }
+
+            _context.Receitas.Remove(receita);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReceitaExists(int id)
+        {
+            return _context.Receitas.Any(e => e.Id == id);
+        }
+    }
+}
